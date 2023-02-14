d2849d324f1a77e045efe3018ecf314a90db81e0
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2023-02-14 22:47:38 +0000

    First section of Dashboard complete :sparkles:

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,46 @@
-import React from "react";
+import { useList } from "@pankod/refine-core";
+import { Typography, Box, Stack } from "@pankod/refine-mui";
+import {
+    PieChart,
+    PropertyReferrals,
+    TotalRevenue,
+    PropertyCard,
+    TopAgent
+} from 'components';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <Box>
+            <Typography fontSize={25} fontWeight={700} color="#11142D">
+                Dashboard
+            </Typography>
+            <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+                <PieChart 
+                    title="Properties for sale"
+                    value={684}
+                    series={[75, 25]}
+                    colors={['#475BE8', '#E4B8EF']}
+                />
+                <PieChart 
+                    title="Properties for rent"
+                    value={550}
+                    series={[60, 40]}
+                    colors={['#475BE8', '#E4B8EF']}
+                />
+                <PieChart 
+                    title="Total Customers"
+                    value={5684}
+                    series={[75, 25]}
+                    colors={['#475BE8', '#E4B8EF']}
+                />
+                <PieChart 
+                    title="Properties for cities"
+                    value={555}
+                    series={[75, 25]}
+                    colors={['#475BE8', '#E4B8EF']}
+                />
+            </Box>
+        </Box>
     )
 }
 
